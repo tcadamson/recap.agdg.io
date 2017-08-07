4249b48102a0bed8b949b0195ae4169d409e3696
Taylor Adamson <tayloradamson@outlook.com>
2017-08-06 18:57:36 -0600

    Added a timeline view for recaps over a certain time period, with a link on the homepage

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,6 @@
+var long = { "august": true, "november": true };
+var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+
 function redirect(URL)
 {
 	window.location = URL;
@@ -31,4 +34,53 @@ function urlExists(URL, args, callback)
         	callback.apply(this, [xhr.status, args]);
         }
     });
+}
+
+function pad(num)
+{
+    return (num < 10) ? ("0" + num) : String(num);
+}
+
+function dateData(month)
+{
+    var date = new Date();
+    var year = String(date.getFullYear()).substring(2);
+    var month = pad(month + 1 || date.getMonth() + 1);
+    return { id: year + month, month: months[date.getMonth()] };
+}
+
+function monthLinks(data, nested)
+{
+    var list = newElement("div", { class: "list" });
+    var weeks = long[data.month] ? 5 : 4;
+    for (var i = 1; i <= weeks; i++)
+    {
+        var link = newElement("div", { class: "link" });
+        var h2 = newElement("h2", {}, "...");
+        var URL = "res/recaps/" + data.id + i + "/data.json";
+        links.push({ l: link, URL: "http://recap.agdg.io/view?id=" + data.id + i });
+        urlExists(URL, { id: i }, function(status, args)
+        {
+            if (status == 200)
+            {
+                var index = args.id - 1;
+                var link = links[index].l;
+                var URL = links[index].URL;
+                var a = newElement("a", { href: URL, class: "na" }, "<h2>" + args.id + "</h2>");
+                link.removeChild(link.childNodes[0]);
+                link.setAttribute("class", "link underline");
+                link.appendChild(a);
+            }
+        });
+        link.appendChild(h2);
+        list.appendChild(link);
+    }
+    if (nested)
+    {
+        var lparent = newElement("div", { class: "list" });
+        lparent.appendChild(newElement("div", { class: "month" }, "<h2>" + data.month + "</h2>"));
+        lparent.appendChild(list);
+        return lparent;
+    }
+    return list;
 }
--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,5 @@
 var enter = 13;
 var links = [];
-var weeks = { "july": 4, "august": 5 };
-var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
 
 $(document).ready(function()
 {
@@ -28,48 +26,15 @@ $(document).ready(function()
 	});
 });
 
-function pad(num)
-{
-	return (num < 10) ? ("0" + num) : String(num);
-}
-
-function dateData()
-{
-	var date = new Date();
-	var year = String(date.getFullYear()).substring(2);
-	var month = pad(date.getMonth() + 1);
-	return { month: months[date.getMonth()], id: year + month };
-}
-
 function buildLinks()
 {
 	var date = dateData();
+	var weeks = long[date.month] ? 5 : 4;
 	var lower = document.getElementById("lower");
-	var cq = newElement("div", { class: "content quick" });
-	var list = newElement("div", { class: "list" });
-    cq.appendChild(newElement("h2", { id: "month"}, date.month));
-    for (var i = 1; i <= weeks[date.month]; i++)
-    {
-    	var link = newElement("div", { class: "link", id: "dead" });
-    	var h2 = newElement("h2", {}, "...");
-    	var URL = "res/recaps/" + date.id + i + "/data.json";
-    	links.push({ l: link, URL: "http://recap.agdg.io/view?id=" + date.id + i });
-    	urlExists(URL, { id: i }, function(status, args)
-    	{
-    		if (status == 200)
-    		{
-    			var index = args.id - 1;
-    			var link = links[index].l;
-    			var URL = links[index].URL;
-    			var a = newElement("a", { href: URL, class: "na" }, "<h2>" + args.id + "</h2>");
-    			link.removeChild(link.childNodes[0]);
-    			link.removeAttribute("id");
-    			link.appendChild(a);
-    		}
-    	});
-    	link.appendChild(h2);
-    	list.appendChild(link);
-    }
-    cq.appendChild(list);
-    lower.appendChild(cq);
+	var block = newElement("div", { class: "content block" });
+	var list = monthLinks(date);
+    block.appendChild(newElement("div", { class: "month" }, "<h2>" + date.month + "</h2>"));
+    block.appendChild(list);
+    block.appendChild(newElement("a", { href: "http://recap.agdg.io/timeline", class: "na blink" }, "<h2>timeline</h2>"));
+    lower.appendChild(block);
 }
