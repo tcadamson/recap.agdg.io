0823bd49bdb3708141768d24a5545f8f6958deee
Taylor Adamson <tayloradamson@outlook.com>
2018-01-15 21:09:08 -0700

    Deferred queues and DOM insertion in the timeline module will now work as intended

--- js/module/m3.js
+++ js/module/m3.js
@@ -11,45 +11,53 @@ function format(temp) {
     return f;
 }
 
-function loadMonth(deferred, month, f) {
+function load(deferred, month, f) {
     var label = $("<div>").attr({class: "label", id: f.ym + "a"});
     var box = $("<div>").attr({class: "boxed border", id: f.ym + "b"});
     for (var i = 1; i <= f.w; i++) {
         var a = $("<a>").addClass("dead").html("?").appendTo(box);
         var defer = $.Deferred();
-        queues[f.m].push(defer);
-        queues[f.y].push(defer);
+        queues[f.y][f.m].push(defer);
+        queues[f.y].net.push(defer);
         $.getJSON("res/recap/" + f.ym + i + "/data.json").done(function() {
-            if (!(f.ym + "b" in registry)) {
+            if (!(f.m in registry[f.y])) {
                 label.html($("<p>").html(month));
-                registry[f.ym] = {label: label, box: box};
+                registry[f.y][f.m] = {label: label, box: box};
             }
         }).always(defer.resolve);
         if (i != f.w) a.addClass("spaced");
     }
-    deferred(queues[f.m], function() {
+    buildMonth(deferred, f);
+}
+
+function buildMonth(deferred, f) {
+    deferred(queues[f.y][f.m], function() {
         for (var i = 0; i < arguments.length; i++) {
             if (!$.isFunction(arguments[i].promise)) {
                 var link = "http://recap.agdg.io/view?id=" + f.ym + (i + 1);
-                box.find("> a:eq(" + i + ")").attr({href: link, target: "_blank"}).removeClass("dead").html(i + 1);
+                var a = registry[f.y][f.m].box.find("> a:eq(" + i + ")");
+                a.attr({href: link, target: "_blank"}).removeClass("dead").html(i + 1);
             }
         }
     });
 }
 
-function build(deferred, year, last) {
-    deferred(queues[year], function() {
-        var keys = Object.keys(registry).sort()
+function buildYear(deferred, fixed, anchor) {
+    var year = fixed.attr("id");
+    deferred(queues[year].net, function() {
+        var keys = Object.keys(registry[year]).sort()
         for (var i = 0; i < keys.length; i++) {
-            var key = keys[i];
-            var objects = registry[key];
-            objects.label.appendTo($("#" + key.slice(0, -2)));
+            var month = keys[i];
+            var objects = registry[year][month];
+            if (!keys[i + 1]) {
+                objects.label.addClass("end");
+                objects.box.addClass("end");
+            }
+            objects.label.appendTo(fixed);
             objects.label.after(objects.box);
         }
-        var fixed = $("#" + year);
-        if (!last) fixed.removeClass("end");
-        fixed.children(".label").last().addClass("end");
-        fixed.children(".boxed.border").last().addClass("end");
+        if (fixed.data("last")) fixed.addClass("end");
+        anchor.after(fixed);
     });
 }
 
@@ -68,23 +76,22 @@ define(function(require) {
     var threshold = 4;
     var breaks = $(".break");
     breaks.each(function(index) {
-        var fixed = $("<div>").attr({class: "fixed end", id: f.y});
-        queues[f.y] = [];
+        var fixed = $("<div>").attr({class: "fixed", id: f.y});
+        if (index + 1 == breaks.length) fixed.data("last", true);
+        registry[f.y] = {};
+        queues[f.y] = {net: []};
         for (var i = 0; i <= copy.weeksInYear(); i++) {
             copy.add(moment.weekdays().length, "days");
             temp.w++;
-            if (copy.month() != temp.m) {
-                if (copy.date() > threshold) {
-                    if (copy.year() != temp.y) temp.y = copy.year();
-                    temp.m = copy.month();
-                    temp.w = 1;
-                    queues[f.m] = [];
-                    loadMonth(deferred, moment.months(copy.month() - 1), f);
-                }
+            if (copy.month() != temp.m && copy.date() > threshold) {
+                temp.y = copy.year();
+                temp.m = copy.month();
+                temp.w = 1;
+                queues[f.y][f.m] = [];
+                load(deferred, moment.months(copy.month() - 1), f);
             }
             f = format(temp);
         }
-        $(this).after(fixed);
-        build(deferred, fixed.attr("id"), index == breaks.length - 1);
+        buildYear(deferred, fixed, $(this));
     });
 });
